6d2ddb4c0d288fb13072f924516068abdff54cb9
Vladimir <33979689+AurelVU@users.noreply.github.com>
2021-02-08 22:19:36 +0300

    Fist commit x4

--- CppAndPython/PythonAndCPP.py
+++ CppAndPython/PythonAndCPP.py
@@ -22,13 +22,8 @@ camera = Camera(fig)
 start_time = time.time()
 for i in range(500):
     T = library.run_phis(T, 70, 70)
-
-
-    #im = ax.imshow(T, interpolation='bilinear', cmap=cm.hot,
-    #               origin='lower', extent=[0, 30, 0, 30])
-
-    #a = plt.plot()
     camera.snap()
+
 print("--- %s seconds ---" % (time.time() - start_time))
 animation = camera.animate()
 animation.save('celluloid_minimal.gif', writer = 'imagemagick')
--- CppAndPython/PythonAndCUDA.py
+++ CppAndPython/PythonAndCUDA.py
@@ -10,11 +10,9 @@ from celluloid import Camera
 
 T = [[0] * 30] * 30
 
-
 delta = 0.1
 x = y = np.arange(0, delta * 30, delta)
 X, Y = np.meshgrid(x, y)
-
 
 fig, ax = plt.subplots()
 
@@ -26,12 +24,12 @@ solver = iter(solver)
 start_time = time.time()
 
 for solve in solver:
-    pass
-    #im = ax.imshow(solve, interpolation='bilinear', cmap=cm.hot,
-    #               origin='lower', extent=[0, len(solve), 0, len(solve)])
-    #a = plt.plot()
-    #camera.snap()
+    # pass
+    im = ax.imshow(solve, interpolation='bilinear', cmap=cm.hot,
+                   origin='lower', extent=[0, len(solve), 0, len(solve)])
+    a = plt.plot()
+    camera.snap()
 
 print("--- %s seconds ---" % (time.time() - start_time))
 animation = camera.animate()
-animation.save('celluloid_minimal.gif', writer = 'imagemagick')
+animation.save('celluloid_minimal.gif', writer='imagemagick')
