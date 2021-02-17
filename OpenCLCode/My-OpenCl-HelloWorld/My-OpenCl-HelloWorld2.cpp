adac3909cd7f52201006818983b88c6d0c4b9a30
Владимир <eau120676@yandex.ru>
2021-02-18 01:55:09 +0300

    Added opencl version

---
+++ OpenCLCode/My-OpenCl-HelloWorld/My-OpenCl-HelloWorld2.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <fstream>
+#include <CL/cl.hpp>
+
+int main() {
+	int n = 100;
+
+	//get all platforms (drivers)
+	std::vector<cl::Platform> all_platforms;
+	cl::Platform::get(&all_platforms);
+	if (all_platforms.size() == 0) {
+		std::cout << " No platforms found. Check OpenCL installation!\n";
+		exit(1);
+	}
+	cl::Platform default_platform = all_platforms[1];
+	std::cout << "Using platform: " << default_platform.getInfo<CL_PLATFORM_NAME>() << "\n";
+
+	//get default device of the default platform
+	std::vector<cl::Device> all_devices;
+	default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
+	if (all_devices.size() == 0) {
+		std::cout << " No devices found. Check OpenCL installation!\n";
+		exit(1);
+	}
+	cl::Device default_device = all_devices[0];
+	std::cout << "Using device: " << default_device.getInfo<CL_DEVICE_NAME>() << "\n";
+
+
+	cl::Context context({ default_device });
+
+	std::ifstream helloWorldFile("hello.cl");
+	std::string src(std::istreambuf_iterator<char>(helloWorldFile), (std::istreambuf_iterator<char>()));
+
+	cl::Program::Sources sources(1, std::make_pair(src.c_str(), src.length() + 1));
+
+	cl::Program program(context, sources);
+	if (program.build({ default_device }) != CL_SUCCESS) {
+		std::cout << " Error building: " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(default_device) << "\n";
+		exit(1);
+	}
+
+
+	// create buffers on the device
+	cl::Buffer buffer_A(context, CL_MEM_READ_WRITE, sizeof(int) * n);
+	cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, sizeof(int) * n);
+	cl::Buffer buffer_C(context, CL_MEM_READ_WRITE, sizeof(int) * n);
+
+	int* A = new int[n];
+	int* B = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		A[i] = i;
+		B[i] = i * 1000;
+	}
+
+	//create queue to which we will push commands for the device.
+	cl::CommandQueue queue(context, default_device);
+
+	//write arrays A and B to the device
+	queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, sizeof(int) * n, A);
+	queue.enqueueWriteBuffer(buffer_B, CL_TRUE, 0, sizeof(int) * n, B);
+
+
+	//run the kernel
+	cl::Kernel simple_add(program, "simple_add");
+	simple_add.setArg(0, buffer_A);
+	simple_add.setArg(1, buffer_B);
+	simple_add.setArg(2, buffer_C);
+	queue.enqueueNDRangeKernel(simple_add, cl::NullRange, cl::NDRange(n), cl::NullRange);
+	queue.finish();
+
+
+	int* C = new int[n];
+	//read result C from the device to array C
+	queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, sizeof(int) * n, C);
+
+	std::cout << " result: \n";
+	for (int i = 0; i < n; i++) {
+		std::cout << C[i] << " ";
+	}
+
+	return 0;
+}
---
+++ OpenCLCode/My-OpenCl-HelloWorld/kernel.cl
@@ -0,0 +1,59 @@
+void kernel solverKernel
+(global float* T, global float* T_new, global int* n, global int* m, global float* h, global float* k, global float* dt, global float* dz, global float* Tb, global float* T0, global float* kL, global float* kR, global float* kU, global float* kD)
+{       
+    int i = get_global_id(0);
+    int j = get_global_id(1);
+
+
+    double cflaxL = 0;
+    double cflaxR = 0;
+    double cflaxU = 0;
+    double cflaxD = 0;
+
+
+
+
+    float h_fluxR = -*k * *h;
+    float h_fluxL = -*k * *h;
+    float h_fluxU = -*k * *h;
+    float h_fluxD = -*k * *h;
+
+    float h_fluxLgran = -*kL * *h * 2;
+    float h_fluxRgran = -*kR * *h * 2;
+    float h_fluxUgran = -*kU * *h * 2;
+    float h_fluxDgran = -*kD * *h * 2;
+
+    float h_fluxC = -4 * *k * *h;
+
+    float h_fluxCLgran = h_fluxR + h_fluxLgran + h_fluxU + h_fluxD;
+    float h_fluxCRgran = h_fluxRgran + h_fluxL + h_fluxU + h_fluxD;
+    float h_fluxCUgran = h_fluxR + h_fluxL + h_fluxUgran + h_fluxD;
+    float h_fluxDUgran = h_fluxR + h_fluxL + h_fluxU + h_fluxDgran;
+
+    float h_fluxCLUgran = h_fluxR + h_fluxLgran + h_fluxUgran + h_fluxD;
+    float h_fluxCRUgran = h_fluxRgran + h_fluxL + h_fluxUgran + h_fluxD;
+    float h_fluxCLDgran = h_fluxR + h_fluxLgran + h_fluxU + h_fluxDgran;
+    float h_fluxDRUgran = h_fluxRgran + h_fluxL + h_fluxU + h_fluxDgran;
+
+
+    if (i == 0)
+        cflaxL = h_fluxLgran * (*Tb - T[i * *m + j]);
+    else
+        cflaxL = h_fluxL * (T[(i - 1) * *m + j] - T[i * *m + j]);
+    if (i == *n - 1)
+        cflaxR = h_fluxRgran * (*T0 - T[i * *m + j]);
+    else
+        cflaxR = h_fluxR * (T[(i + 1) * *m + j] - T[i * *m + j]);
+
+    if (j == 0)
+        cflaxD = h_fluxDgran * (*T0 - T[i * *m + j]);
+    else
+        cflaxD = h_fluxD * (T[i * *m + j - 1] - T[i * *m + j]);
+    if (j == *m - 1)
+        cflaxU = h_fluxUgran * (*T0 - T[i * *m + j]);
+    else
+        cflaxU = h_fluxU * (T[i * *m + j + 1] - T[i * *m + j]);
+
+    T_new[i * *m + j] = T[i * *m + j] - *dt * *h * *h * *dz * (cflaxL + cflaxR + cflaxU + cflaxD); 
+    //T_new[i * *m + j] = h_fluxCLUgran;
+}
